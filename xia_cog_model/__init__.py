28abd0a4c891fafbf4005d82321a2f9fef28ebbb
Xiaokai Xia <dddd1007@gmail.com>
2023-08-30 01:03:59 +0800

    Fix bug

--- xia_cog_model/__init__.py
+++ xia_cog_model/__init__.py
@@ -1,6 +1,3 @@
 from .delta_plot import calc_simon_effect
 from .gather_results import load_all_csvs, process_all_participants
-from .model_based_beh_analysis import (
-    model_overlap_plot,
-    compair_volatility_param,
-)
+from .model_based_beh_analysis import model_overlap_plot
